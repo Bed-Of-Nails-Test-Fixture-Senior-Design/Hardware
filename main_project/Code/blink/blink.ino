da1ba7e9973d684b62ba94f7a0a4fe985b81a1d1
Cade Walton <waltoncade@gmail.com>
2021-06-21 21:53:03 -0700

    Final commit of first semester

--- main_project/Code/blink/blink.ino
+++ main_project/Code/blink/blink.ino
@@ -1,13 +1,14 @@
 int LED = 13; 
 void setup() {
   // put your setup code here, to run once:
+  Serial.begin(9600);
   pinMode(LED, OUTPUT);
+  int x = micros();
+  float z = sqrt(14412398.154);
+  int y = micros();
+  Serial.println(y-x);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  digitalWrite(LED, HIGH);
-  delay(250);
-  digitalWrite(LED, LOW);
-  delay(250);
 }
